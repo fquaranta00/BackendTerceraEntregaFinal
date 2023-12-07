62f6e72addae4a5757c450a93a9c066b478f38c3
Fernando Quaranta <fernando.quaranta@claro.com.ar>
2023-12-07 18:23:34 -0300

    limpieza de codigo

--- src/routers/index.router.js
+++ src/routers/index.router.js
@@ -1,36 +1,3 @@
-// import { Router } from 'express';
-
-// const router = Router();
-
-// const privateRouter = (req, res, next) => {
-//   if (!req.isAuthenticated() || !req.session.user) {
-//     return res.redirect('/login');
-//   }
-//   next();
-// };
-
-// const publicRouters = (req, res, next) => {
-//   if (req.session.user) {
-//     return res.redirect('/views/products');
-//   }
-//   next();
-// }
-
-// router.get('/profile', privateRouter, (req, res) => {
-//   // console.log("ESTOY ADENTRO DE PROFILE ROUTER");
-//   res.render('profile', { title: 'Perfil', user: req.session.user });
-// });
-
-// router.get('/login', publicRouters, (req, res) => {
-//   res.render('login', { title: 'Login' });
-// });
-
-// router.get('/register', publicRouters, (req, res) => {
-//   res.render('register', { title: 'Register' });
-// });
-
-
-// export default router;
 
 import { Router } from 'express';
 import { createHash, verifyPassword, tokenGenerator, verifyToken } from '../utils.js';
